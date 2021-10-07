86a02148d585f8087fc78b858299be1fd38d3b0f
Abdullah Riaz <abdriaz1994@gmail.com>
2021-10-07 21:39:24 +0000

    capitalized query cmds to meet sql style standard

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -2,7 +2,7 @@
 SELECT cpu_number,
        host_id,
        total_mem,
-       row_number() over(partition by cpu_number order by total_mem desc)
+       row_number() OVER(PARTITION BY cpu_number ORDER BY total_mem DESC)
 FROM host_info
 INNER JOIN host_usage on host_info.id=host_usage.host_id;
 
@@ -20,7 +20,7 @@ SELECT host_id,
        round5(hu.timestamp) as rts,
        avg(hi.total_mem-memory_free)/avg(hi.total_mem)*100 as avg_used_memory_percentage
 FROM host_info hi
-INNER JOIN host_usage hu on hi.id=hu.host_id
+INNER JOIN host_usage hu ON hi.id=hu.host_id
 GROUP BY host_id, rts
 ORDER BY rts;
 
