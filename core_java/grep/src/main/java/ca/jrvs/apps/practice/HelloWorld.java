99be4d98b2119023673707b9f7a48f580cbffcb2
Abdullah Riaz <abdullahriaz@Abdullahs-MacBook-Pro.local>
2021-10-24 18:16:49 -0400

    New practice files for Java Regex API

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -0,0 +1,13 @@
+package ca.jrvs.apps.practice;
+
+class HelloWorld {
+
+  // Your program begins with a call to main().
+  // Prints "Hello, World" to the terminal window.
+  public static void main(String args[]) {
+    //new RegexExcImp().matchJpeg("xyz.jpeg");
+   //new RegexExcImp().matchIp("192.dald.fa.ada");
+    new RegexExcImp().isEmptyLine("     h");
+
+  }
+}
