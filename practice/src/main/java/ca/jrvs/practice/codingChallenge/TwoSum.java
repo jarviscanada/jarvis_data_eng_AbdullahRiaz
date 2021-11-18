1be2eba719d76578172803cfc24b88f258cf0e2b
therealskyz <therealskyz@gmail.com>
2021-11-17 19:39:16 -0500

    twoSum using HashMap

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSum.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/TwoSum.java
@@ -1,5 +1,53 @@
 package ca.jrvs.practice.codingChallenge;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * ticket: https://www.notion.so/jarvisdev/Two-Sum-b3ae11302333419a9b7c6e19895eca85
+ */
 public class TwoSum {
 
+  /**
+   * Big O: O(n^2)
+   * Justification:  For each element, we try to find its complement by looping
+   * through the rest of the array which takes O(n) time. Therefore, the time complexity is O(n^2).
+   * @param nums   integer array
+   * @param target target valued to be calculated
+   * @return array of indexes that add to target value
+   */
+  public static int[] twoSum(int[] nums, int target) {
+    for (int i=0; i<nums.length; i++){
+      for (int j=i+1; j<nums.length; j++){
+        if (nums[i] + nums[j] == target){
+          int[] result = {i,j};
+          return result;
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Big O: O(n)
+   * Justification: We traverse the list containing n elements only once. Each lookup in the table costs only O(1) time.
+   * @param nums array
+   * @param target target value
+   * @return array of indexes that add to target value
+   */
+  public static int[] twoSumMap(int[] nums, int target) {
+    //Map - Key:Value at index of nums array, Value:Index associated with Value in nums array
+    Map<Integer,Integer> map = new HashMap<>();
+    for (int i = 0; i<nums.length; i++){
+      int cur = nums[i];
+      //cur + x = target
+      int x = target - cur;
+      if (map.containsKey(x)){
+        return new int[] {map.get(x),i};
+      }
+      map.put(cur,i);
+    }
+    return null;
+  }
 }
---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/TwoSumTest.java
@@ -0,0 +1,20 @@
+package ca.jrvs.practice.codingChallenge;
+
+import org.junit.Assert;
+import org.junit.Test;
+public class TwoSumTest {
+
+  @Test
+  public void twoSum() {
+    int[] arr = {2,5,7,11};
+    int[] expected = {0,1};
+    Assert.assertArrayEquals(expected, TwoSum.twoSum(arr,7));
+  }
+
+  @Test
+  public void twoSumMap() {
+    int[] arr = {2,5,7,11};
+    int[] expected = {0,1};
+    Assert.assertArrayEquals(expected, TwoSum.twoSumMap(arr,7));
+  }
+}
