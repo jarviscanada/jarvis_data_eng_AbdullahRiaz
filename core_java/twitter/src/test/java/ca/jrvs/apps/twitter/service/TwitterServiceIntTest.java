f1899702788e517baf4205eb2879f63415b62149
Abdullah Riaz <abdullahriaz@Abdullahs-MBP.ht.home>
2021-12-14 20:32:47 -0500

    Complete service layer implementation

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/service/TwitterServiceIntTest.java
@@ -1,7 +1,5 @@
 package ca.jrvs.apps.twitter.service;
 
-import static org.junit.Assert.*;
-
 import ca.jrvs.apps.twitter.dao.CrdDao;
 import ca.jrvs.apps.twitter.dao.TwitterDao;
 import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
@@ -10,6 +8,7 @@ import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.util.JsonUtil;
 import ca.jrvs.apps.twitter.util.TweetUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -37,7 +36,7 @@ public class TwitterServiceIntTest {
 
   @Test
   public void postTweet() throws JsonProcessingException {
-    String text = "Testing #Twitter REST API's!!! @skyz786";
+    String text = "Testing #Twitter REST API's!! @skyz786";
     Double lat = 43.83671324;
     Double lon = -79.2511409;
     Tweet postTweet = TweetUtil.buildTweet(text, lon, lat);
@@ -48,16 +47,18 @@ public class TwitterServiceIntTest {
 
   @Test
   public void showTweet() throws JsonProcessingException {
-    String id = "1470488103735500805";
-    String[] fields = {"created_at","id","entities"};
+    String id = "1470923181112406017";
+    String[] fields = {"created_at", "id", "entities"};
     Tweet tweet = service.showTweet(id, fields);
     System.out.println(JsonUtil.toPrettyJson(tweet));
   }
 
   @Test
   public void deleteTweets() throws JsonProcessingException {
-    String[] ids = {"1470813317157597184"};
-    Tweet tweet = (Tweet) service.deleteTweets(ids);
-    System.out.println(JsonUtil.toPrettyJson(tweet));
+    String[] ids = {"1469424871948050437", "1469422486714470404"};
+    List<Tweet> tweets = service.deleteTweets(ids);
+    for (Tweet tweet : tweets) {
+      System.out.println(JsonUtil.toPrettyJson(tweet));
+    }
   }
 }
